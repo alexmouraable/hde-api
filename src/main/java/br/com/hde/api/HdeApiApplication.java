ce0a3b65a5aaf9e63d8b308c39dbded3b671ace1
unknown <alexmoura658@gmail.com>
2025-04-13 23:23:06 -0300

    Initial commit

---
+++ src/main/java/br/com/hde/api/HdeApiApplication.java
@@ -0,0 +1,12 @@
+package br.com.hde.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HdeApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HdeApiApplication.class, args);
+	}
+}
---
+++ src/test/java/br/com/hde/api/HdeApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.hde.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HdeApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
